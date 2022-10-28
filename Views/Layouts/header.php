eb4cdbceaca2f5cb5d664b679a6b8d3838313445
juliang14 <110753708+juliang14@users.noreply.github.com>
2022-10-28 13:05:40 -0500

    Feature - Video 8

--- Views/Layouts/header.php
+++ Views/Layouts/header.php
@@ -10,7 +10,7 @@ $classContactos = ($class==="Index" && $method ==="contactos")?"nav-link active"
 
 ?>
 <div class="container-logo col-12 col-sm-12 col-md-3 col-lg-3 col-xl-3 col-xxl-2 m-auto p-0">
-	<img src="Assets/img/Logo.jpeg" class="d-block m-auto" id="img-logo" alt="logo">
+	<img src="Assets/img/logos/color.png" class="d-block m-auto" id="img-logo" alt="logo">
 </div>
 <div class="container-menu-top col-12 col-sm-12 col-md-9 col-lg-9 col-xl-9 col-xxl-10 p-0">
 	<div class="row ">
