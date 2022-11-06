fc65abc5e3b4a7d4cc2ca7eaef3853f40dbbe798
juliang14 <110753708+juliang14@users.noreply.github.com>
2022-11-06 01:00:08 -0500

    Feature - seccion about
    Se publica la seccion acerca del proyecto

--- Views/User/aboutProject.php
+++ Views/User/aboutProject.php
@@ -18,7 +18,175 @@
         <?php include_once('Views/Layouts/header.php') ?>
     </header>
     <main class="main row m-auto">
-        Acerca del proyecto
+    <section class="col-12 col-sm-12 col-md-10 col-lg-10 col-xl-10 col-xxl-10 m-auto mt-5 mb-5">
+            <h3 class="tittle-login text-center">
+                <span>PROYECTO DE VIVIENDA DE INTERÉS SOCIAL Y PRIORITARIO VIP - VIS “BELLAVISTA”</span>
+            </h3>
+            <ul class="list-group mt-5 mb-5 list-about">
+                <li class="list-group-item"><span class="color-blue">1390</span> unidades de vivienda.</li>
+                <li class="list-group-item"><span class="color-blue">3 urbanizaciones independientes</span> con portería, parqueaderos, zonas comunes, salón comunal, parques infantiles y escenarios deportivos.</li>
+                <li class="list-group-item"></li>
+                <li class="list-group-item"><span class="color-blue">Urbanización Bellavista 2:</span> 580 viviendas VIP y 100 VIS</li>
+                <li class="list-group-item"><span class="color-blue">Urbanización Bellavista 1:</span> 470 viviendas VIS</li>
+                <li class="list-group-item"><span class="color-blue">Bellavista Reservado:</span> 240 viviendas VIS</li>
+                <li class="list-group-item"></li>
+                <li class="list-group-item"><span class="color-blue">Constructor:</span> CONSORCIO PLAN DE VIVIENDA TUNJA VIP-VIS</li>
+                <li class="list-group-item"><span class="color-blue">Fiduciaria:</span> FIDUPOPULAR</li>
+            </ul>
+            <h4 class="tittle-login text-center my-5">
+                <span>PROYECTO BELLAVISTA </span>
+            </h4>
+            <div class="accordion mb-5" id="accordionPanelsStayOpenExample">
+                <div class="accordion-item">
+                    <h2 class="accordion-header" id="panelsStayOpen-headingOne">
+                        <button class="accordion-button collapsed color-blue" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseOne" aria-expanded="false" aria-controls="panelsStayOpen-collapseOne">
+                            OBJETIVO
+                        </button>
+                    </h2>
+                    <div id="panelsStayOpen-collapseOne" class="accordion-collapse collapse" aria-labelledby="panelsStayOpen-headingOne">
+                        <div class="accordion-body">
+                            <p>Dar solución a las familias damnificadas de los proyectos Torres del Parque y Estancia del Roble 
+                                y a las personas que no cuentan con vivienda propia.  
+                                <ul>
+                                    <li>Disminuir el déficit habitacional de la población más vulnerable de la ciudad.</li>
+                                </ul>
+                            </p>
+                            <p>Luego de 12 años de incertidumbre y bajo la premisa de convertir a Tunja en Una Nueva Capital, la 
+                                Administración del alcalde Alejandro Fúneme a través de Ecovivienda, estructura el proyecto de 
+                                vivienda de interés social y prioritario (VIP-VIS) “Bellavista” como alternativa de solución para 
+                                las familias caracterizadas en el grupo 3 de priorización, afectadas por los proyectos inconclusos 
+                                Torres del Parque y Estancia del Roble y para las familias que anhelan el sueño de tener vivienda 
+                                propia.
+                            </p>
+                            <p>El proyecto está inscrito ante el Ministerio de Vivienda, Ciudad y Territorio, con quienes se realizó 
+                                la fase de estructuración y va dirigido a familias que no superen los 4 SMLV de ingresos en sus 
+                                hogares y que cumplan los requisitos para aplicar al programa Nacional “Mi Casa Ya”. Bellavista 
+                                cuenta con licencia de urbanismo y construcción concedida a partir de la Resolución N° 0415 del 07 
+                                de julio de 2022.
+                            </p>
+                            <p>Su ejecución se va a desarrollar en tres urbanizaciones independientes con portería, parqueaderos, 
+                                zonas comunes, salón comunal, parques infantiles y escenarios deportivos; denominadas Bellavista 1, 
+                                Bellavista 2 y Reservado, que comprenden alrededor de 1390 unidades de vivienda, iniciando con la 
+                                construcción de la urbanización Bellavista 2 que contará con 580 viviendas VIP Y 100 viviendas VIS.
+                            </p>
+                            <p>La convocatoria está abierta desde el mes de abril a la comunidad en general que desee realizar el 
+                                proceso de postulación, aclarando que el Consorcio Plan de Vivienda Tunja VIP-VIS seguirá atendiendo 
+                                a todas las familias priorizadas de los proyectos Torres del Parque y Estancia del Roble, hasta 
+                                cuando se agote la oferta comercial de unidades de vivienda.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="accordion-item">
+                    <h2 class="accordion-header" id="panelsStayOpen-headingTwo">
+                        <button class="accordion-button collapsed color-blue" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseTwo" aria-expanded="false" aria-controls="panelsStayOpen-collapseTwo">
+                            MESA DE DIÁLOGO PROYECTOS TORRES DEL PARQUE Y ESTANCIA DEL ROBLE
+                        </button>
+                    </h2>
+                    <div id="panelsStayOpen-collapseTwo" class="accordion-collapse collapse" aria-labelledby="panelsStayOpen-headingTwo">
+                        <div class="accordion-body">
+                            <p>A través de una visita técnica de verificación de avance de obra a los Proyecto Torres del Parque y 
+                                Estancia del Roble, y de una mesa de diálogo desarrollada en presencia de veedurías y entes de 
+                                control, se atendieron los requerimientos de la comunidad partiendo de la gestión adelantada ante 
+                                el Gobierno Nacional para dar solución a los proyectos de vivienda inconclusos.  
+                            </p>
+                            <p>La Contraloría General de la Nación, trabaja de manera articulada con la Administración Municipal, 
+                                el Ministerio de Vivienda, Ciudad y Territorio, y Ecovivienda, en la búsqueda de soluciones 
+                                definitivas para los beneficiarios que resultaron afectados por esta situación que durante 12 años 
+                                ha estado presente en las familias tunjanas que anhelan tener una vivienda digna.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div class="accordion-item">
+                    <h2 class="accordion-header" id="panelsStayOpen-headingThree">
+                        <button class="accordion-button collapsed color-blue" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseThree" aria-expanded="false" aria-controls="panelsStayOpen-collapseThree">
+                            ACTO PROTOCOLARIO INAUGURAL PROYECTO BELLAVISTA
+                        </button>
+                    </h2>
+                    <div id="panelsStayOpen-collapseThree" class="accordion-collapse collapse" aria-labelledby="panelsStayOpen-headingThree">
+                        <div class="accordion-body">
+                            <p>Mediante un acto protocolario inaugural, la Administración Municipal de Tunja a través de la Empresa
+                                 Constructora de Vivienda de Tunja - Ecovivienda y el Consorcio Plan de Vivienda Tunja VIP-VIS, 
+                                 dieron inicio al proyecto de Vivienda de Interés Social VIS y Vivienda de Interés Prioritaria VIP 
+                                 - Bellavista. 
+                            </p>
+                            <p>El evento se desarrolló en el marco de la celebración de los 483 años de fundación hispánica de la 
+                                ciudad de Tunja, reafirmando el compromiso de la administración del alcalde Alejandro Fúneme con la 
+                                población más vulnerable, afectada por los proyectos inconclusos Torres del Parque y Estancia del 
+                                Roble, que a la fecha completaban más de 12 años sin alternativas de solución.
+                            </p>
+                            <p>En esta oportunidad, las más de 50 familias invitadas pertenecientes al grupo 3 de priorización de 
+                                los proyectos en mención y que ya se encuentran vinculadas al proyecto Bellavista, se convirtieron 
+                                en las protagonistas del acto protocolario, contando con un espacio de participación y reflexión, 
+                                donde las entidades que intervienen en el proceso se convirtieron en garantes de tan anhelado sueño 
+                                de los beneficiarios de tener una vivienda digna.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div class="accordion-item">
+                    <h2 class="accordion-header" id="panelsStayOpen-headingFour">
+                        <button class="accordion-button collapsed color-blue" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseFour" aria-expanded="false" aria-controls="panelsStayOpen-collapseFour">
+                            AVANCES DE OBRA
+                        </button>
+                    </h2>
+                    <div id="panelsStayOpen-collapseFour" class="accordion-collapse collapse" aria-labelledby="panelsStayOpen-headingFour">
+                        <div class="accordion-body">
+                            <p><strong>27% de avance de obra - primera etapa:</strong> El alcalde Alejandro Fúneme junto a miembros 
+                                del gabinete municipal, el Honorable Concejo de la ciudad y la Empresa Constructora de Vivienda de Tunja 
+                                – Ecovivienda, realizaron visita técnica de verificación del estado actual de la obra del proyecto de 
+                                vivienda de interés social y prioritario ‘Bellavista’ en su primera fase de ejecución.
+                            </p>
+                            <p>El proyecto que le otorgará la posibilidad de tener vivienda digna a más de 1.300 familias, llegó a 
+                                un avance del 27% en su primera etapa con la fase de cimentación.
+                            </p>
+                            <p><strong>38% de avance de obra - primera etapa:</strong> Tras un nuevo recorrido y seguimiento al área 
+                                de intervención, se evidenció que la obra actualmente alcanza un progreso del 38% con 4 torres en 
+                                ejecución en su primera etapa, en las cuales se han realizado actividades de cimentación, replanteo, 
+                                localización, instalación de redes hidrosanitarias, obras perimetrales, placas de contrapiso y muros.
+                            </p>
+                            <p>Se espera poder entregar las primeras unidades de vivienda a las familias priorizadas finalizando el 
+                                año en curso.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div class="accordion-item">
+                    <h2 class="accordion-header" id="panelsStayOpen-headingFive">
+                        <button class="accordion-button collapsed color-blue" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseFive" aria-expanded="false" aria-controls="panelsStayOpen-collapseFive">
+                            VINCULACIÓN AL PROYECTO
+                        </button>
+                    </h2>
+                    <div id="panelsStayOpen-collapseFive" class="accordion-collapse collapse" aria-labelledby="panelsStayOpen-headingFive">
+                        <div class="accordion-body">
+                            <p>Desde el 08 de mayo del 2022, se dio apertura oficial a la convocatoria para postularse al proyecto de 
+                                vivienda de interés social y prioritario VIP-VIS Bellavista, para que los interesados puedan acercarse 
+                                a la constructora VALU ubicada en la Calle 20 #8-15, plazoleta Pila del Mono, donde les brindarán toda 
+                                la información referente al proceso de vinculación y los requisitos documentales necesarios para su 
+                                postulación.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div class="accordion-item">
+                    <h2 class="accordion-header" id="panelsStayOpen-headingSix">
+                        <button class="accordion-button collapsed color-blue" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseSix" aria-expanded="false" aria-controls="panelsStayOpen-collapseSix">
+                            UBICACIÓN
+                        </button>
+                    </h2>
+                    <div id="panelsStayOpen-collapseSix" class="accordion-collapse collapse" aria-labelledby="panelsStayOpen-headingSix">
+                        <div class="accordion-body">
+                            <p>Se encuentra ubicado en el área urbana del municipio de Tunja dentro de la doble calzada Bogotá - Sogamoso
+                                 y colindando con el Barrio Pinos de Oriente en la Avenida circunvalar #6-110 Sur.
+                            </p>
+                            <div class="text-center">
+                                <img src="assets/img/proyecto_bellavista/about-map.png" alt="img-mapa"/>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
     </main>
     <footer class="footer row">
         <?php include_once('Views/Layouts/footer.php') ?>
